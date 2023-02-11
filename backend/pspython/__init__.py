029d13fd217271112178332a7aa3b7de132739d1
Everson Magalhães <eversonm@alu.ufc.br>
2023-02-10 23:01:52 -0300

    feat: implement jwt token

--- backend/pspython/__init__.py
+++ backend/pspython/__init__.py
@@ -1,8 +1,10 @@
 import os
 
 import models
+from blocklist import BLOCKLIST
 from db import db
-from flask import Flask
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
 from flask_smorest import Api
 
 
@@ -24,9 +26,10 @@ def create_app(test_config=None):
         "DATABASE_URL",
         "sqlite:///data.db"
     )
-
+    app.config["JWT_SECRET_KEY"] = "4314987211440494425041130277451580228"
+    
     db.init_app(app)
-
+    jwt = JWTManager(app)
     with app.app_context():  # new form of: @app.before_first_request
         db.create_all()
 
@@ -42,6 +45,52 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):  # Remember identity is what we define when creating the access token
+        if identity == 1:   # instead of hard-coding, we should read from a config file or database to get a list of admins instead
+            return {'is_admin': True}
+        return {'is_admin': False}
+
+    # This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload['jti'] in BLOCKLIST  # Here we blacklist particular JWTs that have been created in the past.
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return jsonify({
+            "description": "The token has been revoked.",
+            'error': 'token_revoked'
+        }), 401
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return jsonify({
+            "description": "The token is not fresh.",
+            'error': 'fresh_token_required'
+        }), 401
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return jsonify({
+            'message': 'The token has expired.',
+            'error': 'token_expired'
+        }), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):  # we have to keep the argument here, since it's passed in by the caller internally
+        return jsonify({
+            'message': 'Signature verification failed.',
+            'error': 'invalid_token'
+        }), 401
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({
+            "description": "Request does not contain an access token.",
+            'error': 'authorization_required'
+        }), 401
 
     api = Api(app)
 
--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,6 +1,8 @@
 from blocklist import BLOCKLIST
 from db import db
 from flask.views import MethodView
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                get_jwt, get_jwt_identity, jwt_required)
 from flask_smorest import Blueprint, abort
 from models import UserModel
 from passlib.hash import pbkdf2_sha256
@@ -30,6 +32,45 @@ class UserRegister(MethodView):
             return {"message": "User created successfully."}, 201
         except:
             return {"message": "something went wrong"}
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.email == user_data["email"]
+        ).first()
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+
+            return {
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }
+
+        abort(401, message="Invalid credentials.")
+
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+
+        return {"access_token": new_token}
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+
+        return {"message": "Successfully logged out!"}
 
 
 @blp.route("/user/<int:user_id>")
