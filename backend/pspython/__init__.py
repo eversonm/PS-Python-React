f6f2c0e8096ba7ae3a63d76f73f0536dc69ec7f9
Everson Magalhães <eversonm@alu.ufc.br>
2023-02-12 18:42:38 -0300

    feat: implement models and schema

--- backend/pspython/__init__.py
+++ backend/pspython/__init__.py
@@ -1,16 +1,19 @@
 import os
 
-import models
 from blocklist import BLOCKLIST
+from connection import POSTGRES
 from db import db
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from flask_smorest import Api
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "PS Python React"
@@ -22,16 +25,20 @@ def create_app(test_config=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///data.db"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    # os.getenv(
+    #     "DATABASE_URL",
+    #     "sqlite:///data.db"
+    # )
     app.config["JWT_SECRET_KEY"] = "4314987211440494425041130277451580228"
     
     db.init_app(app)
+    migrate = Migrate(app, db)
     jwt = JWTManager(app)
-    with app.app_context():  # new form of: @app.before_first_request
-        db.create_all()
+
+    # @app.before_first_request
+    # def create_tables():
+    #     db.create_all()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -94,8 +101,12 @@ def create_app(test_config=None):
 
     api = Api(app)
 
+    from resources.order import blp as OrderBlueprint
+    from resources.product import blp as ProductBlueprint
     from resources.user import blp as UserBlueprint
     api.register_blueprint(UserBlueprint)
+    api.register_blueprint(ProductBlueprint)
+    api.register_blueprint(OrderBlueprint)
 
     
     return app
